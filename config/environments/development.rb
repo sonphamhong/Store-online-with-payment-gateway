0ed1422398a501f47b030c18f69c178fc1168b52
Sonph <evilsp4@gmail.com>
2015-09-20 11:03:44 +0700

    send mail order to user

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,17 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    user_name: ENV["SMTP_USER"],
+    password: ENV["SMTP_PASSWORD"],
+    domain: "evilsp4.net",
+    authentication: :plain,
+    enable_starttls_auto: false
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
