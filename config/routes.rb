cafbd54b3f5b9aab7699d2e6d0241ddab4104a25
Sonph <evilsp4@gmail.com>
2015-09-20 10:18:14 +0700

    create order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  get 'orders/create'
+
+  devise_for :users
+
   resources :products
+  
   resource :cart do
     post "add", path: "add/:id"
+    get "checkout"
+  end
+
+  resources :orders, only: [ :index, :show, :create, :update ] do
+    member do
+      get :new_payment
+      post :pay
+    end
   end
   root 'products#index'
 
